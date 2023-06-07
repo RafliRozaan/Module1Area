75db259c2c6f476ca11dde7d98f2c61b9ec3b41b
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-07 14:08:52 +0800

    awdawd

--- pages/Input.py
+++ pages/Input.py
@@ -317,7 +317,7 @@ if bg_image is not None:
     c_img, ri = crop_image_v2(np.asarray(image),(crop_sizes,crop_sizes),255)
     image = Image.fromarray(uncrop_image_v2(c_img,ri).astype('uint8'), 'RGB')
     width, height = image.size
-    max_length = 800
+    max_length = 1500
     if height > max_length:
         ratio = max_length / float(height)
         width = int(ratio * width)
